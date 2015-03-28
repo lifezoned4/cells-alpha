01045f8f567ed738a1cd6b7ff494c54c1ba488c7
Michael Solinski <root@livinggames.org>
2015-03-28 17:44:05 +0100

    Self hosting static files webserver in cellsComm.dart

--- cellsserver/cellsComm.dart
+++ cellsserver/cellsComm.dart
@@ -1,7 +1,9 @@
 library comm;
+
 import 'package:logging/logging.dart';
 import 'package:logging_handlers/server_logging_handlers.dart';
-import 'package:logging_handlers/logging_handlers_shared.dart'; 
+import 'package:logging_handlers/logging_handlers_shared.dart';
+import 'package:http_server/http_server.dart' show VirtualDirectory;
 
 import 'dart:convert';
 import 'dart:io';
@@ -12,84 +14,115 @@ import 'cellsProtocolServer.dart';
 final _logger = new Logger("cellsComm");
 
 final String _ip = "127.0.0.1";
-final int _port = 8086;
+final int _portWS = 8086;
+final int _portHTTP = 8087;
 
+VirtualDirectory virDir;
 
 ServerCommEngine _serverCommEngine;
 
-main(){
-    Logger.root.level = Level.INFO;
-    var loggerStream = Logger.root.onRecord.asBroadcastStream();
-    loggerStream.listen(new SyncFileLoggingHandler("saves/logging"));
-    loggerStream.listen(new LogPrintHandler());
-    _logger.info("Starting up Cells Communication Layer");
-   _logger.info("WebServer will be listening on ${_ip}:${_port}");
+_sendNotFound(HttpResponse response) {
+  response.statusCode = HttpStatus.NOT_FOUND;
+  response.close();
+}
 
-   runZoned((){
-   _serverCommEngine = new ServerCommEngine();
-   try {
-   HttpServer.bind(_ip, _port)
-   .then((HttpServer server) {
-     try {
-     server.listen((HttpRequest request) {
-       if(request.uri.path == "/ws"){
-        try {
-         var sc = new StreamController();
-         sc.stream.transform(new WebSocketTransformer())
-          .listen(onWebSocketConn)
-           .onError((err)  => print("Error working on HTTP server: $err"));
-         sc.add(request);
-         sc.done.catchError((error) => _logger.warning("Known WebSocket Error"));
-        } catch(ex) {
-          _logger.warning("Error on WebSocket creation", ex);
-        }
-       } else if (request.uri.path == "/commands" && request.method == 'POST'){
-          Encoding.getByName("ASCII").decodeStream(request).then(
-                                                              (t){_logger.fine("Request POST: " + t);
-                                                                  String reponse =_serverCommEngine.dealWithRestful(t);
-                                                                  request.response.headers.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-                                                                  request.response.headers.set("Access-Control-Allow-Origin","*");
-                                                                  request.response.add(Encoding.getByName("ASCII").encoder.convert(reponse));
-                                                                  request.response.close();
-                                                              });
-          request.response.done.catchError((error) => _logger.warning("Known RESTFul Error"));
-          }
-       else if (request.uri.path == "/version"){
-         request.response.write("ver.0.0.1");
-         request.response.close();
-       }
-       else {
-         try {
-          _logger.warning("Uri not found: ${request.uri.path}");
-          request.response.headers.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-          request.response.headers.set("Access-Control-Allow-Origin","*");
-          request.response.close();
-         } catch (ex) {
-           _logger.warning("Error on response close", ex);
-         }
-       }
-     }, onError: (err){
-       _logger.warning("Listen HttpServer Stream Error!");
-     });
-     }
-     catch(ex) {
-       _logger.warning("Something went wrong inside of HttpServer binding!", ex);
-     }
-   }, onError: (err){
-     _logger.warning("Binding HttpServer Stream Error: $err");
-   });
-   }
-   catch(ex) {
-     _logger.warning("Binding HttpServer failed!", ex);
-   }
+startServerHTTP() async {
+  HttpServer server = await HttpServer.bind(_ip, _portHTTP);
+  _logger.info("HTTP WebServer Listening on ${_ip}:${_portHTTP}");
+  await for (HttpRequest request in server) {
+         virDir.serveRequest(request);
   }
-  , onError: (dynamic err) {
+}
+
+main() {
+  Logger.root.level = Level.INFO;
+  var loggerStream = Logger.root.onRecord.asBroadcastStream();
+  loggerStream.listen(new SyncFileLoggingHandler("saves/logging"));
+  loggerStream.listen(new LogPrintHandler());
+
+  _logger.info("Starting up Cells Communication Layer");
+
+  File script = new File(Platform.script.toFilePath());
+
+  
+  virDir = new VirtualDirectory(script.parent.path + "/../cellsweb/build/web")
+    ..allowDirectoryListing = true;
+
+  startServerHTTP();
+
+  _logger.info("WebSocket will be listening on ${_ip}:${_portWS}");
+
+  runZoned(() {
+    _serverCommEngine = new ServerCommEngine();
+
     try {
-      _logger.shout("Zoned Error: $err");    }
-    catch(ex){
+      HttpServer.bind(_ip, _portWS).then((HttpServer server) {
+        try {
+          server.listen((HttpRequest request) {
+            if (request.uri.path == "/ws") {
+              try {
+                var sc = new StreamController();
+                sc.stream
+                    .transform(new WebSocketTransformer())
+                    .listen(onWebSocketConn)
+                    .onError(
+                        (err) => _logger.warning("Error working on HTTP server: $err"));
+                sc.add(request);
+                sc.done.catchError(
+                    (error) => _logger.warning("Known WebSocket Error"));
+              } catch (ex) {
+                _logger.warning("Error on WebSocket creation", ex);
+              }
+            } else if (request.uri.path == "/commands" &&
+                request.method == 'POST') {
+              Encoding.getByName("ASCII").decodeStream(request).then((t) {
+                _logger.fine("Request POST: " + t);
+                String reponse = _serverCommEngine.dealWithRestful(t);
+                request.response.headers.set("Access-Control-Allow-Headers",
+                    "Origin, X-Requested-With, Content-Type, Accept");
+                request.response.headers.set(
+                    "Access-Control-Allow-Origin", "*");
+                request.response
+                    .add(Encoding.getByName("ASCII").encoder.convert(reponse));
+                request.response.close();
+              });
+              request.response.done.catchError(
+                  (error) => _logger.warning("Known RESTFul Error"));
+            } else if (request.uri.path == "/version") {
+              request.response.write("ver.0.0.1");
+              request.response.close();
+            } else {
+              try {
+                _logger.warning("Uri WebSocket on Port ${_portWS} not found: ${request.uri.path}");
+                request.response.headers.set("Access-Control-Allow-Headers",
+                    "Origin, X-Requested-With, Content-Type, Accept");
+                request.response.headers.set(
+                    "Access-Control-Allow-Origin", "*");
+                request.response.close();
+              } catch (ex) {
+                _logger.warning("Error WebSocket on Port ${_portWS} on response close", ex);
+              }
+            }
+          }, onError: (err) {
+            _logger.warning("Listen HttpServer WebSocket on Port ${_portWS} Stream Error!");
+          });
+        } catch (ex) {
+          _logger.warning(
+              "Something went wrong inside of HttpServer WebSocket on Port ${_portWS} binding!", ex);
+        }
+      }, onError: (err) {
+        _logger.warning("Binding HttpServer for WebSocket on Port ${_portWS} Stream Error: $err");
+      });
+    } catch (ex) {
+      _logger.warning("Binding HttpServer for WebSocket on Port ${_portWS} failed!", ex);
+    }
+  }, onError: (dynamic err) {
+    try {
+      _logger.shout("Zoned Error: $err");
+    } catch (ex) {
       _logger.shout("Yaw Dog, Zoned Zoned Error");
     }
-   });
+  });
 }
 
 onWebSocketConn(WebSocket conn) {
@@ -102,9 +135,8 @@ class CellsWebSocketConnection {
 
   onWebSocketMsg(message) {
     try {
-    _serverCommEngine.dealWithWebSocket(message, conn);
-    }
-    on SocketException catch(ex){
+      _serverCommEngine.dealWithWebSocket(message, conn);
+    } on SocketException catch (ex) {
       _logger.warning("Someting on the WebSocket went wrong", ex);
     }
   }
